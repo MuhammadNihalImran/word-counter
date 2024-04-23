697acd1ff61bb1ceb1bda141d35179aa71431b12
muhammadnihal <work.muhammadnihal@gmail.com>
2024-04-23 04:35:26 -0700

    word counter

---
+++ index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "Sentence",
+        message: "Enter a sentence",
+        validate: (value) => {
+            if (value) {
+                return true;
+            }
+            else {
+                return "Please enter a sentence";
+            }
+        },
+    },
+    {
+        type: "list",
+        name: "ConversionType",
+        message: "Select the conversion type",
+        choices: ["Uppercase", "Lowercase", "Capitalize"],
+    },
+]);
+const words = answer.Sentence.trim().split(" ");
+let convertedWords;
+switch (answer.ConversionType) {
+    case "Uppercase":
+        convertedWords = words.map((word) => word.toUpperCase());
+        break;
+    case "Lowercase":
+        convertedWords = words.map((word) => word.toLowerCase());
+        break;
+    case "Capitalize":
+        convertedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+        break;
+    default:
+        convertedWords = words;
+        break;
+}
+console.log("Original words:", words);
+console.log(`${answer.ConversionType} words:`, convertedWords);
+console.log(`Your sentence word count is ${words.length}`);
